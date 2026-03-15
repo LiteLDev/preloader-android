5bc0cf9b9c218971e7f2c8b8dfdf7f90eea06d5f
alvinqid <github@alvinq.my.id>
2026-03-15 07:12:55 +0700

    fix or test!

--- src/pl/Logger.h
+++ src/pl/Logger.h
@@ -4,45 +4,71 @@
 #include <format>
 #include <string>
 #include <string_view>
+#include <unordered_map>
+#include <memory>
+#include <mutex>
 
 namespace pl::log {
 
-    class Logger {
-    public:
-        explicit Logger(std::string name) : loggerName(std::move(name)) {}
+class Logger {
+public:
+    explicit Logger(std::string name) : loggerName(std::move(name)) {}
 
-        template <typename... Args>
-        void info(std::string_view fmt_str, Args &&...args) const {
-            log(ANDROID_LOG_INFO, fmt_str, std::forward<Args>(args)...);
+    static Logger& getOrCreate(std::string name) {
+        std::lock_guard lock(loggerMutex);
+
+        auto it = loggers.find(name);
+        if (it != loggers.end()) {
+            return *it->second;
         }
 
-        template <typename... Args>
-        void debug(std::string_view fmt_str, Args &&...args) const {
-            log(ANDROID_LOG_DEBUG, fmt_str, std::forward<Args>(args)...);
-        }
+        auto logger = std::make_unique<Logger>(std::move(name));
+        auto& ref = *logger;
+        loggers[ref.loggerName] = std::move(logger);
 
-        template <typename... Args>
-        void warn(std::string_view fmt_str, Args &&...args) const {
-            log(ANDROID_LOG_WARN, fmt_str, std::forward<Args>(args)...);
-        }
+        return ref;
+    }
 
-        template <typename... Args>
-        void error(std::string_view fmt_str, Args &&...args) const {
-            log(ANDROID_LOG_ERROR, fmt_str, std::forward<Args>(args)...);
-        }
+    template <typename... Args>
+    void info(std::string_view fmt_str, Args&&... args) const {
+        log(ANDROID_LOG_INFO, fmt_str, std::forward<Args>(args)...);
+    }
 
-    private:
-        std::string loggerName;
+    template <typename... Args>
+    void debug(std::string_view fmt_str, Args&&... args) const {
+        log(ANDROID_LOG_DEBUG, fmt_str, std::forward<Args>(args)...);
+    }
 
-        template <typename... Args>
-        void log(int android_level, std::string_view fmt_str, Args &&...args) const {
-            auto msg = std::vformat(fmt_str, std::make_format_args(args...));
+    template <typename... Args>
+    void warn(std::string_view fmt_str, Args&&... args) const {
+        log(ANDROID_LOG_WARN, fmt_str, std::forward<Args>(args)...);
+    }
 
-            __android_log_print(android_level, loggerName.c_str(), "%s %s",
-                                loggerName.c_str(), msg.c_str());
-        }
-    };
+    template <typename... Args>
+    void error(std::string_view fmt_str, Args&&... args) const {
+        log(ANDROID_LOG_ERROR, fmt_str, std::forward<Args>(args)...);
+    }
+
+private:
+    std::string loggerName;
+
+    inline static std::unordered_map<std::string, std::unique_ptr<Logger>> loggers{};
+    inline static std::mutex loggerMutex{};
+
+    template <typename... Args>
+    void log(int android_level, std::string_view fmt_str, Args&&... args) const {
+        std::string msg = std::vformat(fmt_str, std::make_format_args(args...));
+
+        __android_log_print(
+            android_level,
+            loggerName.c_str(),
+            "%s",
+            msg.c_str()
+        );
+    }
+};
 
 } // namespace pl::log
 
-inline pl::log::Logger preloader_logger("Preloader");
+
+inline auto& preloader_logger = pl::log::Logger::getOrCreate("Preloader");
